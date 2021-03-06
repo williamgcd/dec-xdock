5d6ef9b925ec4295b14ad2fa39495ab61dd95d81
William Daflita <william.daflita@gmail.com>
2021-03-06 11:39:27 -0300

    Add build to repo

---
+++ build/static/js/53.fe814ae6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdec-xdock"]=this["webpackJsonpdec-xdock"]||[]).push([[53],{161:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e(15),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.fe814ae6.chunk.js.map
